365ddf9f6f0947c49201d2010709f3a016b82101
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-07-07 12:30:17 -0300

    projeto base - react

---
+++ otakutrack-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='underline'>Navbar</h2>
+      <h3>Body</h3>
+      <h4>Footer</h4>
+    </>
+  )
+}
+
+export default App
