01e2ecf524d5dd06f3ff8deda84472878b8a1c53
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-07-08 23:33:00 -0300

    feat: navbar (#16)
    * feat: navbar
    
    navbar criada com itens que podem ser adicionados dinamicamente

--- otakutrack-react/src/App.jsx
+++ otakutrack-react/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './App.css'
+import Navbar from './Navbar'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import CreateUser from './pages/CreateUser'
 
 function App() {
   return (
-    <>
-      <h2 className='underline'>Navbar</h2>
-      <h3>Body</h3>
-      <h4>Footer</h4>
-    </>
-  )
+    <BrowserRouter>
+
+        <Navbar />
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-account" element={<CreateUser />} />
+        </Routes>
+        
+        <h4>Footer</h4>
+
+    </BrowserRouter>
+  );
 }
 
 export default App
