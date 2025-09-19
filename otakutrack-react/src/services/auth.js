e23ed373e5613a537f3495f8d83d358621713b44
Victor <victorgabrielcastrosilva2015@gmail.com>
2025-09-19 17:34:59 -0300

    axios-services

---
+++ otakutrack-react/src/services/auth.js
@@ -0,0 +1,37 @@
+import api from "./api";
+
+// Registrar usuário
+export const register = async (name, email, password) => {
+    const response = await api.post("/auth/register", {
+        name,
+        email,
+        password,
+    });
+    return response.data;
+};
+
+// Login
+export const login = async (email, password) => {
+    const response = await api.post("/auth/login", {
+        email,
+        password,
+    });
+
+    // Salvar o token no localStorage (ou sessionStorage)
+    localStorage.setItem("token", response.data.token);
+
+    return response.data;
+};
+
+// Buscar usuário logado (/me)
+export const getMe = async () => {
+    const token = localStorage.getItem("token");
+
+    const response = await api.get("/me", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return response.data;
+};
